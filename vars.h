ccad2d0c3a6a6f05bf9c13f1ae0c27d731e00ca1
poevoid <65972401+poevoid@users.noreply.github.com>
2021-04-19 13:03:47 -0600

    Update vars.h
    Moved strngs to arrays, created enum class listing each question and answer, to refer to each string when called and put into the buffer

--- vars.h
+++ vars.h
@@ -1,7 +1,21 @@
 #pragma once
 
+
 bool P1correct = false;
 bool P2correct = false;
+
+enum QA: uint8_t {
+  Question1,//0
+  q1UP,
+  q1LEFT,
+  q1DOWN,
+  q1RIGHT,
+  Question2, //5
+  q2UP,
+  q2LEFT,
+  q2DOWN,
+  q2RIGHT,
+};
 
 enum class GameState : uint8_t {
   Title, //0
@@ -22,16 +36,3 @@ int P1score=0;
 int P2score=0;
 int timerbar = 10000;
 int questioncounter = 1;
-
-char question1[] = "Dennis is Asshole. \n Why Charlie hate?";
-char q1ansUP[] = "Because Dennis is a Bastard Man"; //true answer
-char q1ansLEFT[] = "The 'Incident'";
-char q1ansDOWN[] = "Dennis not good vote for Philadelphia, so didnt";
-char q1ansRIGHT[] = "No resp3ct 4 bird law, salmonella";
- 
-
-char question2[] = "What is the solution to the bathroom problem?";
-char q2ansUP[] = "Genderfluid";
-char q2ansLEFT[] = "Gender Neutral";
-char q2ansDOWN[] = "Dick pics & roast beef sandwhiches";
-char q2ansRIGHT[] = "Animal Shithouse"; //true answer
