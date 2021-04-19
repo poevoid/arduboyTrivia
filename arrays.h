0235ad4cb5ddaad97d25e05b376f8ec979c146ce
poevoid <65972401+poevoid@users.noreply.github.com>
2021-04-19 13:00:56 -0600

    Update arrays.h
    Fixed! I must've blanked when first making this, the solution to the arrows being displayed all grokked up was obvious and definitely something I would have realized if I'd looked at the arrays a bit closer. Oops.
    
    Added strings here because they are not variables, didn't make sense to go in vars.h (y'all ever want to yell at your past self for not leaving more comments and ask wtf they're thinkin? Every damn time. :/)

--- arrays.h
+++ arrays.h
@@ -1,16 +1,39 @@
 
 const uint8_t UP[] PROGMEM = {
+  8, 8,
 0x18,0x1C,0xFE,0xFF,0xFF,0xFE,0x1C,0x18,
  };
 
 const uint8_t RIGHT[] PROGMEM = {
-0x18,0x1C,0xFE,0xFF,0xFF,0xFE,0x1C,0x18,
+  8, 8,
+0x3C,0x3C,0x3C,0xFF,0xFF,0x7E,0x3C,0x18,
  };
 
 const uint8_t DOWN[] PROGMEM = {
-0x18,0x1C,0xFE,0xFF,0xFF,0xFE,0x1C,0x18,
+  8, 8,
+0x18,0x38,0x7F,0xFF,0xFF,0x7F,0x38,0x18,
  };
 
 const uint8_t LEFT[] PROGMEM = {
-0x18,0x1C,0xFE,0xFF,0xFF,0xFE,0x1C,0x18,
+  8, 8,
+0x18,0x3C,0x7E,0xFF,0xFF,0x3C,0x3C,0x3C,
  };
+
+
+
+const static char question1[] PROGMEM = {"Dennis is Asshole. \n Why Charlie hate?"};
+const static char q1ansUP[] PROGMEM = {"Because Dennis is a Bastard Man"}; //true answer
+const static char q1ansLEFT[] PROGMEM = {"The 'Incident'"};
+const static char q1ansDOWN[] PROGMEM = {"Dennis not good vote for Philadelphia, so didnt"};
+const static char q1ansRIGHT[] PROGMEM = {"No resp3ct 4 bird law, salmonella"};
+ 
+
+char buffer[55];
+
+const static char question2[] PROGMEM = {"What is the solution to the bathroom problem?"}; //5
+const static char q2ansUP[] PROGMEM = {"Genderfluid"};
+const static char q2ansLEFT[] PROGMEM = {"Gender Neutral"};
+const static char q2ansDOWN[] PROGMEM = {"Dick pics & roast beef sandwhiches"};
+const static char q2ansRIGHT[] PROGMEM = {"Animal Shithouse"}; //true answer //9
+
+const char *const string_table[] PROGMEM = {question1, q1ansUP, q1ansLEFT, q1ansDOWN, q1ansRIGHT, question2, q2ansUP, q2ansLEFT, q2ansDOWN, q2ansRIGHT};
